35f5b8dfe661d0e08cdea6a7e3c06994bd0919e9
KD5VMF <54245381+KD5VMF@users.noreply.github.com>
2024-03-16 21:49:49 -0500

    Wrover-Basic(Good).py

--- Wrover-Basic(Good).py
+++ Wrover-Basic(Good).py
@@ -30,7 +30,7 @@ This software is released under the MIT License.
 
 MIT License
 
-Copyright (c) <year> <copyright holders>
+Copyright (c) <2024> <FigTroNiX>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this
 software and associated documentation files (the "Software"), to deal in the Software
